396fa47dcb1b6b5efe1b75aabde41cf59ac513e0
omuralydev24 <omuralyzarylbekov85@gmail.com>
2024-08-17 12:27:27 +0300

    first commit

--- js/scrip.js
+++ js/scrip.js
@@ -1,47 +1,50 @@
 // omuralyzarylbekov@gmaill.com
 //12345
 
-const email = document.querySelector(".input-email")
-const password = document.querySelector(".input-password")
-const btn = document.querySelector(".btn")
-const text = document.querySelector(".text")
+const email = document.querySelector(".input-email");
+const password = document.querySelector(".input-password");
+const btn = document.querySelector(".btn");
+const text = document.querySelector(".text");
 
-const form = document.querySelector(".form")
+const form = document.querySelector(".form");
 
-let attempt = 3
+let attempt = 3;
 
-form.addEventListener('submit',(e)=>{
-    e.preventDefault()
-    if(email.value === 'omuralyzarylbekov@gmaill.com' && password.value === '2222'){
-        text.innerText = 'Добро пожаловать'
-        resetValue()
-        textColor("blue")
-    }else {
-        attempt--
-        if(attempt === 2){
-            text.innerText = `${attempt} попытки`
-            resetValue()
-            textColor("orange")
-        }else if (attempt === 1){
-            text.innerText=`${attempt} попытка!`
-            resetValue()
-            textColor('red')
-        }else{
-            text.innerText = 'Доступ закырыт!'
-            btn.setAttribute('disabled'," ")
-            resetValue()
-            textColor("gray")
-        }
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (
+    email.value === "omuralyzarylbekov@gmaill.com" &&
+    password.value === "2222"
+  ) {
+    text.innerText = "Добро пожаловать";
+    resetValue();
+    textColor("blue");
+  } else {
+    attempt--;
+    if (attempt === 2) {
+      text.innerText = `${attempt} попытки`;
+      resetValue();
+      textColor("orange");
+    } else if (attempt === 1) {
+      text.innerText = `${attempt} попытка!`;
+      resetValue();
+      textColor("red");
+    } else {
+      text.innerText = "Доступ закырыт!";
+      btn.setAttribute("disabled", " ");
+      resetValue();
+      textColor("gray");
     }
-})
+  }
+});
 
-function textColor(color){
-    text.style.color = color
-    email.style.border = `2px solid ${color}`
-    password.style.border = `2px solid ${color}`
+function textColor(color) {
+  text.style.color = color;
+  email.style.border = `2px solid ${color}`;
+  password.style.border = `2px solid ${color}`;
 }
 
-function resetValue(){
-    email.value = ''
-    password.value = ''
+function resetValue() {
+  email.value = "";
+  password.value = "";
 }
